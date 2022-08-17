f1184ae120417a9d8e3a803fd1815f41dc5e0e93
Nicolas marcaud <nmarcau@gmail.com>
2022-08-17 08:59:37 +0200

    Lecon 36: Intro aux composants

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,13 @@
 <template>
-  <div class="card">
-    <h1>Bonjour {{ state.user.name }}</h1>
-    <p>Count : {{ state.count }}</p>
-    <button class="button" @click="userClick()">Incrémenter</button>
-  </div>
+  <Product />
+  <Product />
+  <Product />
 </template>
 
 <script setup lang="ts">
-  import { reactive } from 'vue';
-
-  const state = reactive({
-    user: {
-      name: 'Nicolas',
-      age:35
-    },
-    count: 0
-  })
-
-  setTimeout(()=> {
-    state.user.name = "Pauline";
-  }, 2000)
-
-
-  function userClick() {
-    state.count++;
-  }
-
+  import Product from './Product.vue';
 </script>
 
-<style>
-.card {
-  background-color:azure;
-  border-radius: 1rem;
-  padding: 1rem;
-  cursor: pointer;
-}
-.button {
-  border-radius: .5rem;
-  padding: .5rem 1rem;
-  border: 0;
-  cursor: pointer;
-}
+<style lang="scss">
+
 </style>
---
+++ src/Product.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card">
+        <h3>Voiture</h3>
+        <ul>
+            <li>Prix : 15 000</li>
+        </ul>
+        <button class="btn" @click="count++">Ajouter au panier</button>
+        <p>Vous avez ajouté {{ count }} voitures</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const count = ref(0);
+</script>
+
+<style scoped lang="scss">
+.card {
+    margin: 1rem;
+    padding: 1.5rem;
+    border-radius: 1rem;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    transition: .4s;
+    &:hover {
+        transform: scale(1.02) translateY(-.2rem);
+    }
+}
+.btn {
+    background-color: royalblue;
+    color: white;
+    padding: .5rem 1.5rem;
+
+    border-radius: .5rem;
+    border: 0;
+    margin: .5rem .5rem .5rem 0;
+    cursor: pointer;
+    transition: .4s;
+
+    &:hover {
+        background-color:cornflowerblue;
+    }
+}
+</style>
